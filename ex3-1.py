cd418a6bdef83c61963de75678d8392b029116be
jinzzam <firstmind_@naver.com>
2025-08-08 09:39:26 +0900

    add if-else ex

---
+++ ex3-1.py
@@ -0,0 +1,8 @@
+x = int(input("정수를 입력하세요 : "))
+# TypeError: '>' not supported between instances of 'str' and 'int'
+
+if x>0 :
+    # print("입력된 수는 양수입니다.")
+# print("입력된 수는 양수입니다.") IndentationError: expected an indented block after 'if' statement on line 4
+    print("입력된 수는 양수입니다.")
+    print("입력된 수는 양수입니다.")
---
+++ ex3-2.py
@@ -0,0 +1,8 @@
+x = int(input("정수를 입력하세요 : "))
+
+# : 자동정렬
+if x%2 == 0:
+    print("짝수이다.")
+# else:
+    # else: # SyntaxError: invalid syntax 들여쓰기가 안됨
+    print("홀수이다.")
