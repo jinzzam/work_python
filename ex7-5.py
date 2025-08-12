c206d79d5d09b30e01be8aee83d9c568652f3d02
jinzzam <firstmind_@naver.com>
2025-08-12 09:15:57 +0900

    some comments

--- ex7-5.py
+++ ex7-5.py
@@ -6,13 +6,15 @@ def func():
     #global x  # 컴파일(?) 할 때 얘 인식 못하나?
     x=100   # 지역변수 : 함수 내에서만 사용 가능
     print(x)
+    return x
 
 # func(10)
 # a = func(10)    # 리턴값이 없을 때 None
 # print(a)
 x=200
 print(x)
-func()
+a = func()
+print("a:",a)
 print(x)
 # 200
 # 100
